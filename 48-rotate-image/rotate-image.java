c53b59792685dcd5c32a8e14f75a5b1eb6d6dfb0
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-03-09 18:24:53 +0900

    Time: 0 ms (100.00%) | Memory: 44.1 MB (13.69%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        //주 대각선 기준으로 대칭한다.
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // y축 반전
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n / 2 ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+
+    }
+}
